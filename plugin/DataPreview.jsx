60f574b83383ca5c58c12c65755b09d537329520
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2022-03-02 09:03:18 +0000

    feat: Add location data

--- plugin/DataPreview.jsx
+++ plugin/DataPreview.jsx
@@ -2,46 +2,49 @@ import React, { useState } from "react";
 import { JSONTree } from "react-json-tree";
 import { Portal } from "./Portal";
 
-import * as containerStyles from "./style.module.css"
+import * as containerStyles from "./style.module.css";
 
 function isEmpty(obj) {
   return Object.keys(obj).length === 0;
 }
 
 const theme = {
-  scheme: 'google',
-  author: 'seth wright (http://sethawright.com)',
-  base00: '#1d1f21',
-  base01: '#282a2e',
-  base02: '#373b41',
-  base03: '#969896',
-  base04: '#b4b7b4',
-  base05: '#c5c8c6',
-  base06: '#e0e0e0',
-  base07: '#ffffff',
-  base08: '#CC342B',
-  base09: '#F96A38',
-  base0A: '#FBA922',
-  base0B: '#198844',
-  base0C: '#3971ED',
-  base0D: '#3971ED',
-  base0E: '#A36AC7',
-  base0F: '#3971ED'
+  scheme: "google",
+  author: "seth wright (http://sethawright.com)",
+  base00: "#1d1f21",
+  base01: "#282a2e",
+  base02: "#373b41",
+  base03: "#969896",
+  base04: "#b4b7b4",
+  base05: "#c5c8c6",
+  base06: "#e0e0e0",
+  base07: "#ffffff",
+  base08: "#CC342B",
+  base09: "#F96A38",
+  base0A: "#FBA922",
+  base0B: "#198844",
+  base0C: "#3971ED",
+  base0D: "#3971ED",
+  base0E: "#A36AC7",
+  base0F: "#3971ED",
 };
 
 export const DataPreview = ({ data }) => {
-
   const [show, setShow] = useState(false);
- 
+
   return (
     <Portal id="preview-blade-portal">
-      <div className={`${containerStyles.pageDataPreviewWrap} ${show ? `${containerStyles.isActive}`: ""}`}>
+      <div
+        className={`${containerStyles.pageDataPreviewWrap} ${
+          show ? `${containerStyles.isActive}` : ""
+        }`}
+      >
         <button
           type="button"
           className={containerStyles.pageDataPreviewAction}
           onClick={() => setShow((s) => !s)}
         >
-          {show ?  '❌' :  '🔨'}
+          {show ? "❌" : "🔨"}
         </button>
         {show && (
           <div className={containerStyles.pageDataPreview}>
@@ -50,34 +53,25 @@ export const DataPreview = ({ data }) => {
               {data?.data && !isEmpty(data.data) && (
                 <div className={containerStyles.pageDataPreviewItem}>
                   <h3>Page Data</h3>
-                  <JSONTree
-                    data={data.data}
-                    theme={theme}
-                    
-                    hideRoot
-                  />
+                  <JSONTree data={data.data} theme={theme} hideRoot />
                 </div>
               )}
               {data?.pageContext && !isEmpty(data.pageContext) && (
                 <div className={containerStyles.pageDataPreviewItem}>
                   <h3>Page Context</h3>
-                  <JSONTree
-                    data={data.pageContext}
-                    theme={theme}
-                    
-                    hideRoot
-                  />
+                  <JSONTree data={data.pageContext} theme={theme} hideRoot />
                 </div>
               )}
               {data?.serverData && !isEmpty(data.serverData) && (
                 <div className={containerStyles.pageDataPreviewItem}>
                   <h3>SSR Data</h3>
-                  <JSONTree
-                    data={data.serverData}
-                    theme={theme}
-                    
-                    hideRoot
-                  />
+                  <JSONTree data={data.serverData} theme={theme} hideRoot />
+                </div>
+              )}
+              {data?.location && !isEmpty(data.location) && (
+                <div className={containerStyles.pageDataPreviewItem}>
+                  <h3>Location Data</h3>
+                  <JSONTree data={data.location} theme={theme} hideRoot />
                 </div>
               )}
             </div>
